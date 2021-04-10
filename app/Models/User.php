53586d60fa938778bb73ec1e22bc76f5421220ee
krystallalala <mind_lab@163.com>
2021-04-10 18:12:29 +0000

    修复用户显示页面css

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,6 @@ class User extends Authenticatable
     public function gravatar($size ='100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
+        return "http://gravatar.zeruns.tech/avatar/$hash?s=$size";
     }
 }
